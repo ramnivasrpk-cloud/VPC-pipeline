ac9615df78f16def4cea1257fdc8cf6df2b71cbf
ramnivasrpk-cloud <ramnivasrpk@gmail.com>
2026-03-28 19:00:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,8 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "ramdev18"
+    key    = "vpc-ec2/terraform.tfstate"
+    region = "ap-south-1a"
+  }
 }
 
 provider "aws" {
-  region = "ap-south-1a"
+  region = "ap-south-1"
 }
