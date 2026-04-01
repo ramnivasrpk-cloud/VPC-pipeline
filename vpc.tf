a1cacab0aef1df764363c8095443e694d3adfa0d
ramnivas <ramnivasrpk@gmail.com>
2026-04-01 19:01:53 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "ap-south-1"
   map_public_ip_on_launch = true
   tags = { Name = "public-subnet" }
 }
@@ -17,7 +17,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-south-1a"
+  availability_zone = "ap-south-1"
   tags = { Name = "private-subnet" }
 }
 
