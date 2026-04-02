727494cd93cdbaeba0e8f92599fd499ce6d0351e
root <root@ip-172-31-47-67.ap-south-1.compute.internal>
2026-04-02 05:54:04 +0000

    change intance

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ubuntu_server" {
   ami           = "ami-05d2d839d4f73aafb" # Ubuntu (ap-south-1)
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.sg.id]
