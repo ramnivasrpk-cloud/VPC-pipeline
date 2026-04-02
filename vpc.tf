67a8ad00bd678ae72806c561695985d4592927d4
ramnivas <ramnivasrpk@gmail.com>
2026-04-02 10:53:59 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
   tags = { Name = "public-subnet" }
 }
